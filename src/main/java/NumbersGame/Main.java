6ae11f2fde3af600893582ab4a1c4ae76fe7db68
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-08 14:59:44 +0300

    all three 3 tasks are here

--- src/main/java/NumbersGame/Main.java
+++ src/main/java/NumbersGame/Main.java
@@ -1,4 +1,31 @@
 package NumbersGame;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Впишите два числа: ");
+        int numNumNum = n.nextInt();
+        int result = n.nextInt();
+        System.out.println("Результирующий счет: " + result);
+
+
+        Nummm(numNumNum, result);
+
+    }
+
+    public static void Nummm(int numNumNum, int result) {
+        List<Integer> numNum = Arrays.asList(1, 2, 3, 4, 8, 1, 9, 0, 5, 3);
+        int i = 9;
+        i++;
+        {
+            numNum.add(i);
+        }
+        numNum.remove(9);
+        System.out.println(result);
+
+    }
 }
